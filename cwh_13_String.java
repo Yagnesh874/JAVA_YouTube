edacbc80dedbde309d5ef952c45986032bc3e31e
Yagnesh874 <142610232+Yagnesh874@users.noreply.github.com>
2023-09-01 20:50:08 +0530

    Add files via upload

---
+++ cwh_13_String.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class cwh_13_String {
+    public static void main(String[] args) {
+        /*
+         String name = new String("hello");
+         String name = "world";
+         System.out.println(name);
+         int a = 5;
+         float b = 6;
+         System.out.printf("The value of a = %d and The value of b = %f \n",a,b);
+         System.out.format("The value of a = %d and the value of b = %f",a,b);
+         */
+         Scanner sc = new Scanner(System.in);
+         String n = sc.nextLine();
+         System.out.println(n);
+
+    }
+}
